ead6aa21f06820348316c0811b8784d10fd886b4
tomc04 <chen.thomas04@gmail.com>
2026-03-16 17:51:15 -0700

    Add remaining links

--- src/App.tsx
+++ src/App.tsx
@@ -461,9 +461,9 @@ export default function App() {
 
   const teamMembers = [
     { name: 'Brian Koh', role: '3rd Year', image: '/images/team_brian.webp', link: 'https://www.linkedin.com/in/briankoh42' },
-    { name: 'David Lym', role: '4th Year', image: '/images/team_david.webp', link: '' },
-    { name: 'Keanu Thakalath', role: 'Senior Software Expert', image: '/images/team_keanu.webp', link: '' },
-    { name: 'Kemin Li', role: '3rd Year', image: '/images/team_kemin.webp', link: '' },
+    { name: 'David Lym', role: '4th Year', image: '/images/team_david.webp', link: 'https://www.linkedin.com/in/david-brandon-lym-33278a24b/' },
+    { name: 'Keanu Thakalath', role: 'Senior Software Expert', image: '/images/team_keanu.webp', link: 'https://www.linkedin.com/in/keanu-thakalath/' },
+    { name: 'Kemin Li', role: '3rd Year', image: '/images/team_kemin.webp', link: 'https://www.linkedin.com/in/kemin-lii/' },
     { name: 'Thomas Chen', role: 'Designer/Developer', image: '/images/team_thomas.webp', link: 'https://www.linkedin.com/in/thomaschen04/' },
   ];
 
